ac1edad194c66936754c8fd6e06e454333a77aa5
mtlogan02 <107326922+mtlogan02@users.noreply.github.com>
2023-12-05 21:49:03 -0500

    express for index, searchMovies, and SearchShows

--- server.js
+++ server.js
@@ -5,3 +5,15 @@ const app = express();
 // Needed for templates
 app.set("views", path.resolve(__dirname, "templates"));
 app.set("view engine", "ejs");
+
+app.get("/", (request, response) => {
+    response.render("index");
+});
+
+app.get("/SearchMovies", (request, response) => {
+    response.render("SearchMovies");
+});
+
+app.get("/SearchShows", (request, response) => {
+    response.render("SearchShows");
+});
