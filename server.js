b29c0998242e6ce0ff18b8df14dc576246b305d6
mtlogan02 <107326922+mtlogan02@users.noreply.github.com>
2023-12-05 21:44:34 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
 const http = require('http');
 const fs = require('fs');
-.....
+const app = express();
+
+// Needed for templates
+app.set("views", path.resolve(__dirname, "templates"));
+app.set("view engine", "ejs");
